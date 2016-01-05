792d5e87a44b31f266850dc7befb931157940223
Zoran Markovic <markonimobile@gmail.com>
2016-01-05 14:39:54 +0100

    DevAppsIT copy files from zm-superCell

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+////////////////////////////////////
+//Probably we should include all js files here like in style.scss
+////////////////////////////////////
+
+/////////// Ovo mi je u stvari DIST gde pozivam sve js fajlove
+
+//Test
+var test123 = require('./script/modules/test/test.js');
+
+test123('Hi Zoran Markovic');
+
+
+console.log('javascript reporting in from app.js ...');
+
+
+
+
+
