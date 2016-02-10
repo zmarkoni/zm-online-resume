94adb46cc73787f415ee8a4fa0f1d22014bf6c2c
Zoran Markovic <markonimobile@gmail.com>
2016-02-10 09:14:08 +0100

    update header

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,14 @@ var size          = require('gulp-size');
 // Javascript Browserify, Watchify, Babel, React
 // https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
 // ////////////////////////////////////////////////
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: "Compile Error",
+    message: "<%= error.message %>"
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
 
 // add custom browserify options here
 var customOpts = {
@@ -32,15 +40,17 @@ var b = watchify(browserify(opts)); //call browserify which put all scripts in o
 gulp.task('js', bundle); // so you can run `gulp js` to build the file
 b.on('update', bundle); // on any dep update, runs the bundler
 b.on('log', gutil.log); // output build logs to terminal
+//b.on('error', handleErrors); Testiraj ovo
 
 function bundle() {
   return b.bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, gutil.colors.red(
-       '\n\n*********************************** \n' +
-      'BROWSERIFY ERROR:' +
-      '\n*********************************** \n\n'
-      )))
+    // log errors if they happen - OVO NE RADI
+    // .on('error', gutil.log.bind(gutil, gutil.colors.red(
+    //    '\n\n*********************************** \n' +
+    //   'BROWSERIFY ERROR:' +
+    //   '\n*********************************** \n\n'
+    //   )))
+    //.on('error', handleErrors) Testiraj ovo
     .pipe(source('main.js'))
     // optional, remove if you don't need to buffer file contents
     .pipe(buffer())
--- src/js/script/modules/header/runner.js
+++ src/js/script/modules/header/runner.js
@@ -13,44 +13,25 @@ var headerRunner = function() { //ne mogu da stavim ime funkcije!
         fixHeaderHeight();
     });
 
-
-
-
-
     function fixHeaderHeight() {
         var windowHeight = $(window).height();
-        $('.header').css({
+        $('.page_header').css({
+            height: windowHeight
+        });
+        $('.header_bg').css({
             height: windowHeight
         });
     }
 
     function scrollHeader() {
-        // var windowScroll = $(window).scrollTop();
-        // console.log('window scroll: ' + windowScroll);
-        // var scrollValue = 'center';
-        // console.log('scrollValue: ' + scrollValue);
-        // if (windowScroll > 20) {
-        //     scrollValue = -windowScroll;
-        // } else if (windowScroll < 20) {
-        //     scrollValue = 'center';
-        // }
-        //
-        // $('.header').animate({
-        //     backgroundPositionY: scrollValue
-        // }, 0, 'linear');
-
-
         // if (docWidth1 > 640) {
             var parallax = $('.parallax');
-
             var st = $(window).scrollTop(); //documentElement.scrollTop
-
             parallax.css({
                 '-webkit-transform': 'translateY(' + -st / 3 + 'px)',
                 '-ms-transform': 'translateY(' + -st / 3 + 'px)',
                 'transform': 'translateY(' + -st / 3 + 'px)'
             });
-
         //}
     }
 
