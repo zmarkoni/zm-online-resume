0325f53fc92dbd7a7726654db6eb4a67794afde8
Zoran Markovic <markonimobile@gmail.com>
2016-03-08 23:28:38 +0100

    update header and navigation for mobile

--- src/js/src/modules/header/events/listener.js
+++ src/js/src/modules/header/events/listener.js
@@ -1,17 +1,21 @@
+var $ = require('jquery');
+var Sandbox = require('../../../sandbox.js');
 var HeaderManager = require('../models/header-manager-new.js');
 
 //Listener izvrsava ono sto cuje
 var Events = function() {
 
-    $(window).on('scroll', function() {
-        new HeaderManager().scrollHeader();
-    });
+    new HeaderManager().fixHeaderHeight();
 
-    $(window).on('resize', function() {
-        new HeaderManager().fixHeaderHeight(); //call on window resize
-    });
-
-    //moze i TRIGER kad nesto odmah hocu da se desi
+    if( Sandbox.screen().isLarge() ) {
+        console.log('big only');
+        $(window).on('scroll', function() {
+            new HeaderManager().scrollHeader();
+        });
+        $(window).on('resize', function() {
+            new HeaderManager().fixHeaderHeight(); //call on window resize
+        });
+    }
 
 };
 
